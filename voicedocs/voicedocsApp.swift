aa79587b51047f033aba94c78b43b2856fb35163
entaku <entaku19890818@gmail.com>
2024-06-01 22:26:25 +0900

    first commit

---
+++ voicedocs/voicedocsApp.swift
@@ -0,0 +1,17 @@
+//
+//  voicedocsApp.swift
+//  voicedocs
+//
+//  Created by 遠藤拓弥 on 2024/06/01.
+//
+
+import SwiftUI
+
+@main
+struct voicedocsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
