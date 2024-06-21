1c662c7fac8a0b36960b2465a8c44301dbe43219
entaku <entaku19890818@gmail.com>
2024-06-21 09:22:56 +0900

    add

--- voicedocs/voicedocsApp.swift
+++ voicedocs/voicedocsApp.swift
@@ -6,9 +6,35 @@
 //
 
 import SwiftUI
+import FirebaseCore
+import Firebase
+
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+
+    #if DEBUG
+      if FirebaseApp.app() != nil {
+          print("Firebase has been successfully configured.")
+      } else {
+          print("Firebase configuration failed.")
+      }
+    Analytics.setAnalyticsCollectionEnabled(true)
+    Analytics.setUserID("debug_user")
+    print("Firebase Analytics debug logging is enabled")
+    #endif
+
+    return true
+  }
+}
 
 @main
 struct SpeechRecognitionApp: App {
+  @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+
+
     var body: some Scene {
         WindowGroup {
             VoiceMemoListView(voiceMemoController: VoiceMemoController())
